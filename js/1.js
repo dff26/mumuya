f98d6344886f3376f4db73d1775cf58bd72ff605
dff26 <59459336+dff26@users.noreply.github.com>
2020-10-22 13:05:58 +0800

    Add files via upload

---
+++ js/1.js
@@ -0,0 +1,16 @@
+window.onload =function() {
+		$("label img").css("margin-top","0");
+		$("#text p").css("opacity","1");
+		$(".latiao p").css("margin-top","0",
+		"opacity","1");
+		//延时器
+		setTimeout(function(){
+			$(".introduzione  h1").css("left","0",
+			"opacity","1");
+		},2400)
+		
+		setTimeout(function(){
+			$(".introduzione  p").css("right","0");
+		},2600)
+		
+	}	
---
+++ js/2.js
@@ -0,0 +1,17 @@
+// 绑定事件
+let divTyping = document.getElementById('text')
+  let i = 0,
+    timer = 0,
+    str = '滚滚'+'长江'+'东逝水';
+  function typing () {
+    // 判断
+	if (i <= str.length) {
+      divTyping.innerHTML = str.slice(0, i++) + '_'
+      timer = setTimeout(typing, 500)
+    }
+    else {
+      divTyping.innerHTML = str//结束打字,移除 _ 光标
+      clearTimeout(timer)
+    }
+  }
+
